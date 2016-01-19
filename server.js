0aa20cb55f3ed376377f5a39bc5db85bc5145eb5
BruceWeng <weng8916@gmail.com>
2016-01-19 01:27:13 -0800

    Middleware and Request Flow

--- server.js
+++ server.js
@@ -2,39 +2,21 @@ var express = require('express');
 // var bodyParser = require('body-parser');
 var app = express();
 
-// Setting Params Configurations
-app.set('env', 'development'); // process.env.NODE_ENV = 'production'
-app.enable('trust proxy');
-app.set('jsonp callback name', 'cb');
-app.set('json replacer', function(attr, val) {
-  if (attr === 'paawordHash') {
-    return undefined;
-  }
-  return val;
+// third-party middleware
+app.use(bodyParser.urlencoded());
+
+// custom middleware
+app.user(function(request, response, next) {
+    console.log("this will log on every request");
+    next();
+});
+// route function
+app.get('/route', function(request, response) {
+    response.send("this is a route");
 });
 
-app.get('/user_info', function(request, response) {
-    // get user data
-    response.json(user); // JSON.stringify
-});
-
-app.enable('case sensitive routing'); // /hello and /hello/hello are different
-app.enable('strict'); // /hello /hello/ are the same
-
-app.enable('view cache');
-app.enable('view engine', 'ejs');
-app.set('views', 'templates');
-app.disable('x-powered-by'); // Remove express tag in Header
-
-
-
-
-
-app.get('/', function(request, response) {
-  request.render('index.ejs', {
-      title: "Hello Express and Jade"
-  });
-});
+// built-in middleware
+app.use(express.static('./public'));
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
