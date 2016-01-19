fb51493ef41e26e7b085b925e33d14aa262ebde4
BruceWeng <weng8916@gmail.com>
2016-01-19 00:50:25 -0800

    The Verb Methods

--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 var express = require('express');
-var app     = express();
+var bodyParser = require('body-parser');
+var app = express();
 
-app.get('/', function(request, response) {
-    response.render('index.ejs', {
-        title: "Hello Express and Jade"
+function log(request, response, next) {
+  console.log(names);
+  next();
+}
+
+app.use(bodyParser.urlencoded());
+
+var names = [];
+
+app.all('/', function(request, response, next) {
+    console.log('from ALL');
+    next();
+})
+
+// GET
+app.get('/', log, function(request, response) {
+    response.render('index.jade', {
+        names: names
     });
 });
 
+// POST
+app.post('/', function(request, response) {
+    names.push(request.body.name);
+    response.redirect('/');
+})
+
 app.listen(3000, function() {
   console.log("Listening on port 3000");
-})
+});
