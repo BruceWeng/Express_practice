f5d26157cb4029939350047fcf630da03a755d28
BruceWeng <weng8916@gmail.com>
2016-01-19 01:15:26 -0800

    Application Setting

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var app = express();
 
-function log(request, response, next) {
-  console.log(names);
-  next();
-}
-
-app.use(bodyParser.urlencoded());
-
-var names = [];
-
-app.all('/', function(request, response, next) {
-    console.log('from ALL');
-    next();
-})
-
-// GET
-app.get('/', log, function(request, response) {
-    response.render('index.jade', {
-        names: names
-    });
+// Setting Params Configurations
+app.set('env', 'development'); // process.env.NODE_ENV = 'production'
+app.enable('trust proxy');
+app.set('jsonp callback name', 'cb');
+app.set('json replacer', function(attr, val) {
+  if (attr === 'paawordHash') {
+    return undefined;
+  }
+  return val;
 });
 
-// POST
-app.post('/', function(request, response) {
-    names.push(request.body.name);
-    response.redirect('/');
-})
+app.get('/user_info', function(request, response) {
+    // get user data
+    response.json(user); // JSON.stringify
+});
+
+app.enable('case sensitive routing'); // /hello and /hello/hello are different
+app.enable('strict'); // /hello /hello/ are the same
+
+app.enable('view cache');
+app.enable('view engine', 'ejs');
+app.set('views', 'templates');
+app.disable('x-powered-by'); // Remove express tag in Header
+
+
+
+
+
+app.get('/', function(request, response) {
+  request.render('index.ejs', {
+      title: "Hello Express and Jade"
+  });
+});
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
