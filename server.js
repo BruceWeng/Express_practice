e8c15a9c37b8d7afba898298c4ad2332135c9712
BruceWeng <weng8916@gmail.com>
2016-01-19 18:17:47 -0800

    Request Object

--- server.js
+++ server.js
@@ -1,34 +1,18 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var APIv1 = require('./api1');
-var APIv2 = require('./api2');
 
-app.use('/api/v1', APIv1);
-app.use('/api/v2', APIv2);
-// Router Object
-// - use
-// - param
-// - verb / all
-// - route
+app.get('/:breedOfDog', function(request, response) {
+    request.params.breedOfDog;
+    request.query.ATTR;
+    request.body.ATTR;
+    request.param('ATTR'); //check in the order of params, body and query
 
-// var router = express.Router();
-
-// router.use(function(request, response, next) {
-//     console.log('router specific middleware');
-//     next();
-// });
-
-// router.get('/', function(resuest, response) {
-//     response.send('router home route');
-// });
-
-// router.route('/test')
-//     .get(function(request, response) {
-//         response.send('router test route');
-// });
-
-// app.use('/api', router);
+    request.route;
+    request.originalUrl;
+    request.cookies.ATTR;
+    request.get(); // get any header name
+    request.accepts('text/html'); // application/json/plain
+});
 
 app.listen(3000, function() {
   console.log("Listening on port 3000");
