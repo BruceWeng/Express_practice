223fa461f577a7cc28f9827742fdaa567e291856
BruceWeng <weng8916@gmail.com>
2016-01-19 18:22:51 -0800

    Response Object

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.get('/:breedOfDog', function(request, response) {
     request.cookies.ATTR;
     request.get(); // get any header name
     request.accepts('text/html'); // application/json/plain
+
+    response.status(200);
+    response.set(header, value);
+    response.get(header);
+    response.cookie(name, value);
+    repsonse.clearCookie(name);
+    response.redirect(status, path);
+    response.send(status, text);
+    response.json(status, object);
+    response.jsonp(status, object); // callback({}) used in cross domain
+    response.download(file);
+    response.render(file, props, function(err, html) {
+        response.send(html);
+    });
+
 });
 
 app.listen(3000, function() {
